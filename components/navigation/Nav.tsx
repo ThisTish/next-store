085436173b44ec62955bda56550b180e7a0cb047
Tish <tishthegoat@gmail.com>
2024-10-28 19:51:01 -0600

    terenary signin/out nav

--- components/navigation/Nav.tsx
+++ components/navigation/Nav.tsx
@@ -1,6 +1,9 @@
 import { auth } from "@/server/auth"
-import Logo from "./Logo";
-import UserBtn from "./UserBtn";
+import Logo from "./Logo"
+import UserBtn from "./UserBtn"
+import { Button } from "../ui/button"
+import Link from "next/link"
+import { LogIn } from 'lucide-react'
 
 const Nav = async () => {
 	const session = await auth()
@@ -8,14 +11,30 @@ const Nav = async () => {
 		<header className="bg-slate-400 py-4">
 			<ul className="flex justify-between">
 				<li>
+					<Link href='/'>
 					<Logo />
+					</Link>
 				</li>
-				<li>
-					<UserBtn expires={session?.expires ?? ''} user={session?.user}/>
-				</li>
+				{!session ? (
+					<li>
+						<Button asChild>
+							<Link href='/auth/login'>
+								<LogIn />
+								<span>Login</span>
+							</Link>
+						</Button>
+					</li>
+				)
+					:
+					(
+						<li>
+							<UserBtn expires={session?.expires ?? ''} user={session?.user} />
+						</li>
+					)
+				}
 			</ul>
 		</header>
-	);
+	)
 }
 
 export default Nav
